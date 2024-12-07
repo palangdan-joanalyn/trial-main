63842b713732e8ff69dc7d6574ae808dd1d05702
Palangdan, J <2216317@slu.edu.ph>
2024-12-07 19:06:50 +0800

    feat(number manipulations): added functions to add and multiply numbers

---
+++ src/trial.erl
@@ -0,0 +1,16 @@
+-module(trial).
+
+
+add_two_numbers(Num1, Num2) ->
+    Num1 + Num2.
+
+multiply_by_three(Num) ->
+
+    Num*3.
+main() -> io:fwrite('Displaying the sum and product of random numbers.'),Sum=add_two_numbers(30,87),Product=multiply_by_three(Sum),io:format("~nThese are the following results after the manipulation of numbers: ~nSum: ~p~n Product:~p~n",Sum,Product).
+
+
+
+-export([run/0]).
+run() ->
+    main().
